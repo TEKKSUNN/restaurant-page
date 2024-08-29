d62540fbeacf97fbf7ae762c26cb8c8cd673e889
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-08-29 11:45:36 +0800

    Make logo & title clickable

--- src/index.js
+++ src/index.js
@@ -2,17 +2,30 @@ import "./stylesheets/styles.css";
 
 import RestaurantLogo from "./images/restaurant.png";
 
+import template from "./template.html";
+
+const createLinkTag = function(url) {
+    const linkTag = document.createElement("a");
+    linkTag.href = url;
+    linkTag.rel = "noopener noreferrer";
+    return linkTag;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const RestaurantInfo = document.getElementById("restaurant-info");
     (function addRestaurantInfo() {
+        const linkTitle = createLinkTag(template);
         const title = document.createElement("h1");
-        title.textContent = "Restaurant";
+        title.textContent = "Haraldr";
         title.id = "restaurant-title";
+        linkTitle.appendChild(title);
+        const linkImage = createLinkTag(template);
         const image = document.createElement("img");
         image.src = RestaurantLogo;
         image.alt = "restaurant logo";
         image.id = "restaurant-logo";
-        RestaurantInfo.appendChild(image);
-        RestaurantInfo.appendChild(title);
+        linkImage.appendChild(image);
+        RestaurantInfo.appendChild(linkImage);
+        RestaurantInfo.appendChild(linkTitle);
     })();
 });
