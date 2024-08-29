fd347dbf5d763abeb662e33a4a2baf8f12d21a64
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-08-29 09:44:50 +0800

    Add support for images & add images

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import "./stylesheets/styles.css";
 
-console.log("Hello");
+import RestaurantLogo from "./images/restaurant.png";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const RestaurantInfo = document.getElementById("restaurant-info");
+    (function addRestaurantInfo() {
+        const title = document.createElement("h1");
+        title.textContent = "Restaurant";
+        title.id = "restaurant-title";
+        const image = document.createElement("img");
+        image.src = RestaurantLogo;
+        image.alt = "restaurant logo";
+        image.id = "restaurant-logo";
+        RestaurantInfo.appendChild(image);
+        RestaurantInfo.appendChild(title);
+    })();
+});
